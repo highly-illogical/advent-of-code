8470e20b0300ccbe92a901cc534dcb5ce8ad2bd8
highly-illogical <prachi1916@gmail.com>
2024-12-15 18:42:04 +0530

    Day 15 Part 2

--- 2024/day15_warehouse_woes/solution.py
+++ 2024/day15_warehouse_woes/solution.py
@@ -73,13 +73,84 @@ class WarehouseWoes:
             # print(self)
             return True
 
+    def is_blocked_expanded(self, move_char):
+        move = self.move_dict[move_char]
+        to_move = [(self.robot_pos_expanded[0], self.robot_pos_expanded[1])]
+
+        while to_move:
+            #print(to_move)
+            for square in to_move:
+                # Check if any squares are blocked directly by the next square
+                i_new, j_new = square[0] + move[0], square[1] + move[1]
+                if self.expanded_map[i_new][j_new] == "#":
+                    return True
+                    
+            to_move_new = []
+            for square in to_move:
+                # Calculate next squares
+                i_new, j_new = square[0] + move[0], square[1] + move[1]
+                # print(square, i_new, j_new, self.expanded_map[i_new][j_new])
+                if self.expanded_map[i_new][j_new] in ["[", "]"]:
+                    to_move_new.append((i_new, j_new))
+                if move_char in ["^", "v"]:
+                    if self.expanded_map[i_new][j_new] == "[":
+                        to_move_new.append((i_new, j_new + 1))
+                    elif self.expanded_map[i_new][j_new] == "]":
+                        to_move_new.append((i_new, j_new - 1))
+
+            to_move = list(set(to_move_new))
+
+        return False
+
+    def copy_to_next(self, i, j, move):
+        if self.map_expanded[i + move[0]][j + move[1]] != ".":
+            self.copy_to_next(i + move[0], j + move[1], move)
+        self.map_expanded[i + move[0]][j + move[1]] = self.map_expanded[i][j]
+        self.map_expanded[i][j] = "."
+
     def move_expanded(self, move_char):
         move = self.move_dict[move_char]
-        return move
         
-    def execute_moves(self):
+        to_move_all = []
+
+        if not self.is_blocked_expanded(move_char):
+            to_move = [(self.robot_pos_expanded[0], self.robot_pos_expanded[1])]
+            
+            while to_move:
+                to_move_all.append(to_move)
+                        
+                to_move_new = []
+                for square in to_move:
+                    # Calculate next squares
+                    i_new, j_new = square[0] + move[0], square[1] + move[1]
+                    if self.expanded_map[i_new][j_new] in ["[", "]"]:
+                        to_move_new.append((i_new, j_new))
+                    if move_char in ["^", "v"]:
+                        if self.expanded_map[i_new][j_new] == "[":
+                            to_move_new.append((i_new, j_new + 1))
+                        elif self.expanded_map[i_new][j_new] == "]":
+                            to_move_new.append((i_new, j_new - 1))
+
+                to_move = list(set(to_move_new))
+        else:
+            #print("Blocked")
+            return False
+        
+        for to_move in to_move_all[::-1]:
+            for square in to_move:
+                i, j = square
+                i_next, j_next = i + move[0], j + move[1]
+                self.expanded_map[i_next][j_next] = self.expanded_map[i][j]
+                self.expanded_map[i][j] = "."
+                if self.expanded_map[i_next][j_next] == "@":
+                    self.robot_pos_expanded = i_next, j_next
+
+    def execute_moves(self, expanded=True):
         for m in self.moves:
-            self.move(m)
+            if not expanded:
+                self.move(m)
+            else:
+                self.move_expanded(m)
 
     def sum_gps_coordinates(self):
         total = 0
@@ -89,9 +160,17 @@ class WarehouseWoes:
                     total += i * 100 + j
         return total
 
+    def sum_gps_coordinates_expanded(self):
+        total = 0
+        for i in range(len(self.expanded_map)):
+            for j in range(len(self.expanded_map[i])):
+                if self.expanded_map[i][j] == "[":
+                    total += i * 100 + j
+        return total
+
 if __name__ == "__main__":
-    w = WarehouseWoes("test2.txt")
+    w = WarehouseWoes("input.txt")
     print(w)
-    #w.execute_moves()
-    #print(w)
-    #print(w.sum_gps_coordinates())
+    w.execute_moves()
+    print(w)
+    print(w.sum_gps_coordinates_expanded())
