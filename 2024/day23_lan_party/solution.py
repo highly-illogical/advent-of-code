1680b3f255fd83c231045c84a5e8abcc51c7ea4c
highly-illogical <prachi1916@gmail.com>
2024-12-23 15:35:49 +0530

    Day 23 Part 2

--- 2024/day23_lan_party/solution.py
+++ 2024/day23_lan_party/solution.py
@@ -1,5 +1,6 @@
 class LANParty:
     def __init__(self, filename):
+        self.connected_components = []
         self._read_input(filename)
 
     def _read_input(self, filename):
@@ -7,6 +8,7 @@ class LANParty:
             self.G = {}
             for line in file:
                 a, b = line.strip("\n").split("-")
+                self.connected_components.append((a, b))
                 if a not in self.G:
                     self.G[a] = [b]
                 else:
@@ -25,11 +27,27 @@ class LANParty:
                         if node3 in self.G[node2] and node3 in self.G[node1]:
                             if node1[0] == 't' or node2[0] == 't' or node3[0] == 't':
                                 triplets.append(tuple(sorted([node1, node2, node3])))
-        print(set(triplets))
+        self.triplets = triplets
         return(len(set(triplets)))
+
+    def next_connected(self):
+        nplus1_connected = []
+
+        for component in self.connected_components:
+            connected_nodes = set(self.G[component[0]])
+            for node in component:
+                connected_nodes = connected_nodes.intersection(self.G[node])
+
+            for cn in connected_nodes:
+                nplus1_connected.append(tuple(sorted([*component, cn])))
+
+        self.connected_components = set(nplus1_connected)
 
 if __name__ == "__main__":
     lp = LANParty("input.txt")
     print(lp.G)
-    triplets = lp.find_3()
-    print(triplets)            
+    
+    while len(lp.connected_components) > 1:
+        lp.next_connected()
+
+    print(",".join(list(lp.connected_components)[0]))
